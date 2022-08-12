6d85dd7d2d2af12781cfa0d4eb8f8387d73f580f
Ivan Wu <ivan.wu@elmosoftware.com.au>
2022-08-13 00:21:00 +1000

    fix the indent

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { task1 } from './task1';
 
 const hello = async (): Promise<void> => {
-  console.log('Hello World');
-  await task1();
+    console.log('Hello World');
+    await task1();
 
 }
 hello();
--- task5.ts
+++ task5.ts
@@ -4,21 +4,21 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from './libs/s3-client';
 
 export const task5 = async (csv: any): Promise<void> => {
-  console.log(`This is task 5`);
-  dotenv.config();
-  console.log(`AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID}`);
-  console.log(`AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY}`);
+	console.log(`This is task 5`);
+    dotenv.config();
+    console.log(`AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID}`);
+    console.log(`AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY}`);
 
-  const uploadParams = {
-    Bucket: 'inquisitive-backend-developer-tests',
-    Key: 'Ivan-Wu.csv',
-    Body: csv,
-  };
+    const uploadParams = {
+    	Bucket: 'inquisitive-backend-developer-tests',
+    	Key: 'Ivan-Wu.csv',
+    	Body: csv,
+    };
 
-  try {
-    const data = await s3Client.send(new PutObjectCommand(uploadParams));
-    console.log('Success', data);
-  } catch (error) {
-    console.log('Error', error);
-  }
+    try {
+    	const data = await s3Client.send(new PutObjectCommand(uploadParams));
+    	console.log('Success', data);
+    } catch (error) {
+    	console.log('Error', error);
+    }
 }
